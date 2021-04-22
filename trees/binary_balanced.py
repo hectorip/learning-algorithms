e86cf450e729013327df5de4c603eff5322572e7
Héctor Iván Patricio Moreno <hectorivanpatriciomoreno@gmail.com>
2021-04-21 23:59:37 -0500

    WIP Tree

--- trees/binary_balanced.py
+++ trees/binary_balanced.py
@@ -1,15 +1,36 @@
-# Binary search Tree is a search tree that helps in search
+"""Binary search Tree is a search tree that helps in search"""
 
 
-class Tree():
+class Tree:
     """Implements all the functions of a tree"""
-   def __init__(self, value, left=None, right=None):
+
+    def __init__(self, value, left=None, right=None):
         self._left = left
         self._right = right
         self.value = value
 
     def add(self, value):
-        if self.value >= value:
-            self._left.add(value)
-        else:
+        if self.value > value:
+            if self._left is not None:
+                self._left.add(value)
+            else:
+                self.left = Tree(value)
+        elif self.value < value:
+            if self._right is not None:
+                self._right.add(value)
+            else:
+                self._right = Tree(value)
+        # Balance Tree
+        # Investigate if it is unbalanced
+        # Balance
 
+    def print(self, level=0):
+        print(self.value)
+        if self._left is not None:
+            print("|")
+            self._left.print()
+
+
+values =  [3, 2, 1]
+
+for v in values:
