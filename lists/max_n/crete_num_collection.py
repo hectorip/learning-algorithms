ce780f0f0892ae5ecf7294650fdf7a47e2fa924a
Héctor Iván Patricio Moreno <hectorivanpatriciomoreno@gmail.com>
2021-05-06 23:59:27 -0500

    WIP

--- lists/max_n/crete_num_collection.py
+++ lists/max_n/crete_num_collection.py
@@ -1,4 +1,4 @@
 # Create a new file with line-separated numbers
-# with open("numbers.txt", "w") as file:
-#     for n in range(0, 100000000):
-#         print(str(n), file=file)
+with open("numbers.txt", "w") as file:
+    for n in range(0, 100000000):
+        print(str(n), file=file)
