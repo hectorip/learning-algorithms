0e89f5e918503bf2cd396b366e48656ed7b4b01d
Héctor Iván Patricio Moreno <hectorivanpatriciomoreno@gmail.com>
2021-05-26 23:49:13 -0500

    Merge lists completed

--- lists/merge/merge.py
+++ lists/merge/merge.py
@@ -4,6 +4,22 @@ class ListNode:
         self.val = val
         self.next = next
 
+    @classmethod
+    def create(cls, array):
+        l = cls(array[0])
+        cl = l
+        for e in array[1:]:
+            nn = ListNode(e)
+            cl.next = nn
+            cl = nn
+        return l
+
+    def print(self):
+        print(self.val)
+        if self.next:
+            print("->")
+            self.next.print()
+
 
 def merge(l1, l2):
     """Merge two ordered lists into one ordered list.
@@ -20,11 +36,37 @@ def merge(l1, l2):
 
 
 def merge_list(l1, l2):
-    while l1:
-        while l2 and (l2.val <= l1.val):
-            l2.next = l1
-            l1 = l2
-            l2 = l2.next.next
+    if not l1:
+        return l2
+    if not l2:
+        return l1
 
-        l1 = l1.next
-    return l1
+    if l1.val <= l2.val:
+        rl = l1
+
+        al = l2
+    else:
+        rl = l2
+        al = l1
+
+    mp = rl
+    ap = al
+    while mp.next:
+        while ap and (ap.val <= mp.next.val):
+            temp = mp.next
+            mp.next = ap
+            mp = mp.next
+
+            ap = ap.next
+            mp.next = temp
+
+        mp = mp.next
+    mp.next = ap
+    return rl
+
+
+if __name__ == "__main__":
+    l1 = ListNode.create([1, 3, 5])
+    # ll.print()
+    l2 = ListNode.create([2, 4, 6])
+    merge_list(l1, l2).print()
