3ef6f9d7400ace73eb28e903624d0d3fa0f5719b
Héctor Iván Patricio Moreno <hectorivanpatriciomoreno@gmail.com>
2021-06-08 23:10:45 -0500

    range_bsp

--- lists/binary/main.py
+++ lists/binary/main.py
@@ -1,2 +1,10 @@
 
-def 
+def list_to_binary(l):
+    """Idea: Get element by element multiplying previous sum by 2 each time"""
+    total = 0
+    while l:
+        total *= 2
+        total += l.val
+        l = l.next
+
+    return total
---
+++ trees/range_bsp.py/main.py
@@ -0,0 +1,11 @@
+
+def range_sum(root, left, right):
+    result = 0
+    nodes = [root]
+    while nodes:
+        node = nodes.pop()
+        if node.val >= left and node.val <= right:
+            result += node.val
+        node.left and nodes.append(node.left)
+        node.right and nodes.append(node.right)
+    return result
