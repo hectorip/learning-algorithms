2ea576cfdf9a9772684597c32c7ad5bf0c908d4d
Héctor Iván Patricio Moreno <hectorivanpatriciomoreno@gmail.com>
2021-06-18 17:47:23 -0500

    Max Subtree

---
+++ trees/max_subtree.py
@@ -0,0 +1,17 @@
+"""
+LeetCode: https://leetcode.com/problems/maximum-binary-tree
+"""
+
+
+def constructMaximumBinaryTree(nums: List[int]):
+    if not nums:
+        return None
+    m = max(nums)
+    mi = nums.index(m)
+    left = nums[:mi]
+    right = nums[mi + 1 :]
+    return TreeNode(
+        val=m,
+        left=constructMaximumBinaryTree(left),
+        right=constructMaximumBinaryTree(right),
+    )
