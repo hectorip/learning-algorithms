3745513840a3c7860005c679ff23aaa1a3fc6f40
Héctor Iván Patricio Moreno <hectorivanpatriciomoreno@gmail.com>
2021-11-12 17:08:42 -0600

    First bad version

---
+++ lists/firstBadVersion/main.py
@@ -0,0 +1,19 @@
+    def firstBadVersion(self, n):
+        """
+        :type n: int
+        :rtype: int
+        """
+        lb = n
+        lg = n//2
+        while lg >= 1 and lg < lb:
+            # print(lg, lb)
+            if isBadVersion(lg):
+                lb = lg
+                lg -= (lg//2)
+            else:
+                i = (lb - lg) // 2
+                if not i:
+                    break
+                lg += i
+            
+        return lb
